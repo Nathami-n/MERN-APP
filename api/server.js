217e36d614425bf1c59d782e9c179590ccf43a11
Nathan George <georgenathan010@gmail.com>
2024-02-25 23:09:02 +0300

    added the database model

--- api/server.js
+++ api/server.js
@@ -1,17 +1,14 @@
 import express from "express";
+import { URI, PORT } from "./config/config.js";
 import { connect } from "./db/connectDb.js";
-import dotenv from 'dotenv'
 const app = express();
-dotenv.config();
-const PORT = process.env.PORT || 3000;
-const URI = process.env.MONGO_URI;
 
 const startServer = async () => {
-    try {
-        await connect(URI);
-        app.listen(PORT, console.log(`listening on ${PORT}`));
-    } catch(e) {
-        console.error(e.message);
-    }
-}
+  try {
+    await connect(URI);
+    app.listen(PORT, console.log(`listening on ${PORT}`));
+  } catch (e) {
+    console.error(e.message);
+  }
+};
 startServer();
