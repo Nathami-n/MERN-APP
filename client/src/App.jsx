f54e834475a45a42ada13f8717f303cf01e6e039
Nathan George <georgenathan010@gmail.com>
2024-02-25 21:54:10 +0300

    added the header

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,11 @@
 import { Route, Routes } from "react-router-dom";
 import { Home, SignIn, SignUp, Profile, About } from "./pages";
+import { Header } from "./components";
 
 const App = () => {
   return (
     <div>
+      <Header />
       <Routes>
         <Route exact path="/" element={<Home />} />
         <Route exact path="/profile" element={<Profile />} />
--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,5 +1,40 @@
+import { Link } from "react-router-dom";
+import {CiSearch} from 'react-icons/ci'
+
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <header className="w-full bg-gray-300">
+      <div className=" p-2 flex justify-between max-w-7xl mx-auto items-center">
+        <div>
+          <h1 className="text-xl font-bold flex justify-center items-center">
+            <span className="">CITA</span>
+            <span className="text-red-600">HOMES</span>
+          </h1>
+        </div>
+        <div>
+          <form className="p-1 flex items-center bg-white rounded-lg">
+            <input
+              type="text"
+              className="p-3 bg-transparent outline-none w-32 sm:w-64"
+              placeholder="search..."
+            />
+            <CiSearch className=" text-xl cursor-pointer"/>
+          </form>
+        </div>
+        <ul className="flex gap-3">
+          <Link to={'/'} className="hidden  text-md sm:text-xl  sm:inline hover:text-blue-500 hover:underline">
+            Home
+          </Link>
+          <Link to={'/about'} className=" sm:inline text-md sm:text-xl hover:text-blue-500 hover:underline">
+            About
+          </Link>
+          <Link to={'/sign-in'} className="sm:inline  text-md sm:text-xl hover:text-blue-500 hover:underline">
+            Sign in
+          </Link>
+        </ul>
+      </div>
+    </header>
+  );
 };
 
 export default Header;
