e505496b454b84200a61cd08b2b4fb1371111b77
Nathan George <georgenathan010@gmail.com>
2024-03-03 13:30:49 +0300

    added the persist redux to the application

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,14 +3,17 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
-import store from "./redux/store/store.jsx";
+import store, { persistor } from "./redux/store/store.jsx";
 import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <BrowserRouter>
     <React.StrictMode>
       <Provider store={store}>
-        <App />
+        <PersistGate loading={null} persistor={persistor}>
+          <App />
+        </PersistGate>
       </Provider>
     </React.StrictMode>
   </BrowserRouter>
--- client/src/redux/store/store.jsx
+++ client/src/redux/store/store.jsx
@@ -1,9 +1,26 @@
 import { configureStore } from "@reduxjs/toolkit";
+import {persistStore, persistReducer} from 'redux-persist'
+import storage from 'redux-persist/lib/storage';
 import formReducer from "../features/user/formSlice";
 import userReducer from '../features/user/userSlice'
-export default configureStore({
-  reducer: {
-    form:formReducer,
-    user:userReducer,
-  },
+
+const userPersistConfig = {
+  key:"user",
+  storage:storage,
+}
+
+const persistedUserReducer = persistReducer(userPersistConfig, userReducer);
+
+const rootReducer = {
+  form:formReducer,
+  user:persistedUserReducer,
+}
+
+const store = configureStore({
+  reducer: rootReducer,
 });
+
+
+export const persistor = persistStore(store);
+
+export default store;
